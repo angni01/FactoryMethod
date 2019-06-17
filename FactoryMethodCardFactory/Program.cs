89a7c0a1fad39ab79cc5b3ad933241cc40bed34e
BlastAsia Cloud02 <baicloud02@outlook.com>
2019-06-17 11:13:53 +0800

    Add project files.

---
+++ FactoryMethodCardFactory/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using FactoryMethodCreditCard;
+
+namespace FactoryMethodCardFactory
+{
+	public abstract class CardFactory
+	{
+		public abstract CreditCard GetCreditCard();
+	}
+
+	public class MoneyBackFactory : CardFactory
+	{
+		private int _creditLimit;
+		private int _annualCharge;
+
+		public MoneyBackFactory(int creditLimit, int annualCharge)
+		{
+			_creditLimit = creditLimit;
+			_annualCharge = annualCharge;
+		}
+
+		public override CreditCard GetCreditCard()
+		{
+			return new MoneyBackCreditCard(_creditLimit, _annualCharge);
+		}
+	}
+
+	public class TitaniumFactory : CardFactory
+	{
+		private int _creditLimit;
+		private int _annualCharge;
+
+		public TitaniumFactory(int creditLimit, int annualCharge)
+		{
+			_creditLimit = creditLimit;
+			_annualCharge = annualCharge;
+		}
+
+		public override CreditCard GetCreditCard()
+		{
+			return new TitaniumCreditCard(_creditLimit, _annualCharge);
+		}
+	}
+
+	public class PlatinumFactory : CardFactory
+	{
+		private int _creditLimit;
+		private int _annualCharge;
+
+		public PlatinumFactory(int creditLimit, int annualCharge)
+		{
+			_creditLimit = creditLimit;
+			_annualCharge = annualCharge;
+		}
+
+		public override CreditCard GetCreditCard()
+		{
+			return new PlatinumCreditCard(_creditLimit, _annualCharge);
+		}
+	}
+}
